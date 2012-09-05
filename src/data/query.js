3a49719b53a9f395416d077b53b865f74a64a0db
Mikael Karon <mikael@karon.se>
2012-09-05 11:54:33 +0800

    added initial query parsing with tests

--- src/data/query.js
+++ src/data/query.js
@@ -1,3 +1,102 @@
 define( [ "troopjs-core/component/base" ], function QueryModule(Component) {
-    return Component;
+    var UNDEFINED;
+    var AST = "ast";
+    var OP = "op";
+    var TEXT = "text";
+
+    return Component.extend(function Query(query) {
+        if (query !== UNDEFINED) {
+            this.parse(query);
+        }
+    }, {
+        parse : function parse(query) {
+            var me = this;
+
+            var l;                  // Length
+            var c;                  // Current character
+            var i;                  // Current index
+            var m;                  // Current mark
+            var q;                  // Current quote
+            var o;                  // Current op
+            var a = me[AST] = [];   // AST
+
+            // Step through the query character by character
+            for (i = m = 0, l = query.length; i < l; i++) {
+                c = query.charAt(i);
+
+                switch (c) {
+                    case "\"" : // Double quote
+                    case "'" :  // Single quote
+                        // Set / unset quote char
+                        q = q === c
+                            ? UNDEFINED
+                            : c;
+                        break;
+
+                    case "!" :  // ID operator
+                        // Break fast if we're quoted
+                        if (q !== UNDEFINED) {
+                            break;
+                        }
+
+                        // Init new op
+                        o = {};
+                        o[OP] = c;
+                        break;
+
+                    case "." :  // Property operator
+                    case ";" :  // Sub-query separator
+                        // Break fast if we're quoted
+                        if (q !== UNDEFINED) {
+                            break;
+                        }
+
+                        // If there's an active op, store TEXT and push on AST
+                        if (o !== UNDEFINED) {
+                            o[TEXT] = query.substring(m, i);
+                            a.push(o);
+                        }
+
+                        // Init new op
+                        o = {};
+                        o[OP] = c;
+
+                        // Set mark
+                        m = i + 1;
+                        break;
+
+                    case "|" :  // Query separator
+                    case " " :  // Space
+                    case "\t" : // Horizontal tab
+                    case "\r" : // Carriage return
+                    case "\n" : // Newline
+                        // Break fast if we're quoted
+                        if (q !== UNDEFINED) {
+                            break;
+                        }
+
+                        // If there's an active op, store TEXT and push on AST
+                        if (o !== UNDEFINED) {
+                            o[TEXT] = query.substring(m, i);
+                            a.push(o);
+                        }
+
+                        // Reset op
+                        o = UNDEFINED;
+
+                        // Set mark
+                        m = i + 1;
+                        break;
+                }
+            }
+
+            // If there's an active op, store TEXT and push on AST
+            if (o !== UNDEFINED) {
+                o[TEXT] = query.substring(m, l);
+                a.push(o);
+            }
+
+            return me;
+       }
+    });
 });
--- test/data/query.js
+++ test/data/query.js
@@ -1,8 +1,166 @@
 buster.testCase("troopjs-ef/data/query", function (run) {
+    var assert = buster.assert;
+    var refute = buster.assertions.refute;
+
     require( [ "troopjs-ef/data/query" ] , function (Query) {
         run({
-            "true" : function () {
-                buster.assert(true);
+            "test!123" : function () {
+                var ast = Query("test!123").ast;
+
+                assert.equals(ast, [{
+                    "op" : "!",
+                    "text" : "test!123"
+                }]);
+            },
+
+            "test!123|xxx!321" : function () {
+                var ast = Query("test!123|xxx!321").ast;
+
+                assert.equals(ast, [{
+                    "op" : "!",
+                    "text" : "test!123"
+                }, {
+                    "op" : "!",
+                    "text" : "xxx!321"
+                }]);
+            },
+
+            "test!123.p1" : function () {
+                var ast = Query("test!123.p1").ast;
+
+                assert.equals(ast, [{
+                    "op" : "!",
+                    "text" : "test!123"
+                }, {
+                    "op" : ".",
+                    "text" : "p1"
+                }]);
+            },
+
+            "test!123.p1.p2" : function () {
+                var ast = Query("test!123.p1.p2").ast;
+
+                assert.equals(ast, [{
+                    "op" : "!",
+                    "text" : "test!123"
+                }, {
+                    "op" : ".",
+                    "text" : "p1"
+                }, {
+                    "op" : ".",
+                    "text" : "p2"
+                }]);
+            },
+
+            "test!123.p1;.p2" : function () {
+                var ast = Query("test!123.p1;.p2").ast;
+
+                assert.equals(ast, [{
+                    "op" : "!",
+                    "text" : "test!123"
+                }, {
+                    "op" : ".",
+                    "text" : "p1"
+                }, {
+                    "op" : ";",
+                    "text" : ""
+                }, {
+                    "op" : ".",
+                    "text" : "p2"
+                }]);
+            },
+
+            "test!123.p1;.p2|xxx!321.p3.p4;.p5" : function () {
+                var ast = Query("test!123.p1;.p2|xxx!321.p3.p4;.p5").ast;
+
+                assert.equals(ast, [{
+                    "op" : "!",
+                    "text" : "test!123"
+                }, {
+                    "op" : ".",
+                    "text" : "p1"
+                }, {
+                    "op" : ";",
+                    "text" : ""
+                }, {
+                    "op" : ".",
+                    "text" : "p2"
+                }, {
+                    "op" : "!",
+                    "text" : "xxx!321"
+                }, {
+                    "op" : ".",
+                    "text" : "p3"
+                }, {
+                    "op" : ".",
+                    "text" : "p4"
+                }, {
+                    "op" : ";",
+                    "text" : ""
+                }, {
+                    "op" : ".",
+                    "text" : "p5"
+                }]);
+            },
+
+            "test!123 .p1;   .p2|xxx!321 .p3  .p4   ; .p5" : function () {
+                var ast = Query("test!123 .p1;   .p2|xxx!321 .p3  .p4   ; .p5").ast;
+
+                assert.equals(ast, [{
+                    "op" : "!",
+                    "text" : "test!123"
+                }, {
+                    "op" : ".",
+                    "text" : "p1"
+                }, {
+                    "op" : ";",
+                    "text" : ""
+                }, {
+                    "op" : ".",
+                    "text" : "p2"
+                }, {
+                    "op" : "!",
+                    "text" : "xxx!321"
+                }, {
+                    "op" : ".",
+                    "text" : "p3"
+                }, {
+                    "op" : ".",
+                    "text" : "p4"
+                }, {
+                    "op" : ";",
+                    "text" : ""
+                }, {
+                    "op" : ".",
+                    "text" : "p5"
+                }]);
+            },
+
+            "test!'123 321'" : function () {
+                var ast = Query("test!'123 321'").ast;
+
+                assert.equals(ast, [{
+                    "op" : "!",
+                    "text" : "test!'123 321'"
+                }]);
+            },
+
+            "test!'123 321'.p1;.'p2 asd'" : function () {
+                var ast = Query("test!'123 321'.p1;.'p2 asd'").ast;
+
+                assert.equals(ast, [{
+                    "op" : "!",
+                    "text" : "test!'123 321'"
+                }, {
+                    "op" : ".",
+                    "text" : "p1"
+                }, {
+                    "op" : ";",
+                    "text" : ""
+                }, {
+                    "op" : ".",
+                    "text" : "'p2 asd'"
+                }]);
             }
         });
     });
