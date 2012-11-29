dbf9c747b25c4663e948d7f5db57c689220cfe3d
Mikael Karon <mikael@karon.se>
2012-11-29 11:46:27 +0800

    spaces -> tabs

--- src/service/query.js
+++ src/service/query.js
@@ -1,217 +1,217 @@
 define([ "../component/service", "../data/query", "troopjs-core/pubsub/topic", "troopjs-utils/deferred", "troopjs-utils/merge" ], function QueryServiceModule(Service, Query, Topic, Deferred, merge) {
-    var UNDEFINED;
-    var ARRAY_PROTO = Array.prototype;
-    var SLICE = ARRAY_PROTO.slice;
-    var CONCAT = ARRAY_PROTO.concat;
-    var PUSH = ARRAY_PROTO.push;
-    var LENGTH = "length";
-    var BATCHES = "batches";
-    var INTERVAL = "interval";
-    var CACHE = "cache";
-    var TOPIC = "topic";
-    var QUERIES = "queries";
-    var RESOLVED = "resolved";
-    var RAW = "raw";
-    var ID = "id";
-    var Q = "q";
+	var UNDEFINED;
+	var ARRAY_PROTO = Array.prototype;
+	var SLICE = ARRAY_PROTO.slice;
+	var CONCAT = ARRAY_PROTO.concat;
+	var PUSH = ARRAY_PROTO.push;
+	var LENGTH = "length";
+	var BATCHES = "batches";
+	var INTERVAL = "interval";
+	var CACHE = "cache";
+	var TOPIC = "topic";
+	var QUERIES = "queries";
+	var RESOLVED = "resolved";
+	var RAW = "raw";
+	var ID = "id";
+	var Q = "q";
 
-    return Service.extend(function QueryService(cache) {
-        var me = this;
+	return Service.extend(function QueryService(cache) {
+		var me = this;
 
-        me[BATCHES] = [];
-        me[CACHE] = cache;
-    }, {
-        displayName : "ef/service/query",
+		me[BATCHES] = [];
+		me[CACHE] = cache;
+	}, {
+		displayName : "ef/service/query",
 
-        "sig/start" : function start(signal, deferred) {
-            var me = this;
-            var cache = me[CACHE];
+		"sig/start" : function start(signal, deferred) {
+			var me = this;
+			var cache = me[CACHE];
 
-            // Set interval (if we don't have one)
-            me[INTERVAL] = INTERVAL in me
-                ? me[INTERVAL]
-                : setInterval(function batchInterval() {
-                var batches = me[BATCHES];
+			// Set interval (if we don't have one)
+			me[INTERVAL] = INTERVAL in me
+				? me[INTERVAL]
+				: setInterval(function batchInterval() {
+				var batches = me[BATCHES];
 
-                // Return fast if there is nothing to do
-                if (batches[LENGTH] === 0) {
-                    return;
-                }
+				// Return fast if there is nothing to do
+				if (batches[LENGTH] === 0) {
+					return;
+				}
 
-                // Reset batches
-                me[BATCHES] = [];
+				// Reset batches
+				me[BATCHES] = [];
 
-                Deferred(function deferredRequest(dfdRequest) {
-                    var q = [];
-                    var topics = [];
-                    var batch;
-                    var i;
+				Deferred(function deferredRequest(dfdRequest) {
+					var q = [];
+					var topics = [];
+					var batch;
+					var i;
 
-                    // Iterate batches
-                    for (i = batches[LENGTH]; i--;) {
-                        batch = batches[i];
+					// Iterate batches
+					for (i = batches[LENGTH]; i--;) {
+						batch = batches[i];
 
-                        // Add batch[TOPIC] to topics
-                        PUSH.call(topics, batch[TOPIC]);
+						// Add batch[TOPIC] to topics
+						PUSH.call(topics, batch[TOPIC]);
 
-                        // Add batch[Q] to q
-                        PUSH.apply(q, batch[Q]);
-                    }
+						// Add batch[Q] to q
+						PUSH.apply(q, batch[Q]);
+					}
 
-                    // No q, might as well resolve
-                    if (q[LENGTH] === 0) {
-                        dfdRequest.resolve(q);
-                    }
-                    // Otherwise request from backend
-                    else {
-                        // Publish ajax
-                        me.publish(Topic("ajax", me, topics), merge.call({
-                            "data": {
-                                "q": q.join("|")
-                            }
-                        }, me.config.api.query), dfdRequest);
-                    }
-                })
-                    .done(function doneRequest(data /* , textStatus, jqXHR */) {
-                        var batch;
-                        var queries;
-                        var id;
-                        var i;
-                        var j;
+					// No q, might as well resolve
+					if (q[LENGTH] === 0) {
+						dfdRequest.resolve(q);
+					}
+					// Otherwise request from backend
+					else {
+						// Publish ajax
+						me.publish(Topic("ajax", me, topics), merge.call({
+							"data": {
+								"q": q.join("|")
+							}
+						}, me.config.api.query), dfdRequest);
+					}
+				})
+					.done(function doneRequest(data /* , textStatus, jqXHR */) {
+						var batch;
+						var queries;
+						var id;
+						var i;
+						var j;
 
-                        // Add all new data to cache
-                        cache.put(data);
+						// Add all new data to cache
+						cache.put(data);
 
-                        // Iterate batches
-                        for (i = batches[LENGTH]; i--;) {
-                            batch = batches[i];
-                            queries = batch[QUERIES];
-                            id = batch[ID];
+						// Iterate batches
+						for (i = batches[LENGTH]; i--;) {
+							batch = batches[i];
+							queries = batch[QUERIES];
+							id = batch[ID];
 
-                            // Iterate queries
-                            for (j = queries[LENGTH]; j--;) {
-                                // If we have a corresponding ID, fetch from cache
-                                if (j in id) {
-                                    queries[j] = cache[id[j]];
-                                }
-                            }
+							// Iterate queries
+							for (j = queries[LENGTH]; j--;) {
+								// If we have a corresponding ID, fetch from cache
+								if (j in id) {
+									queries[j] = cache[id[j]];
+								}
+							}
 
-                            // Resolve batch
-                            batch.resolve.apply(batch, queries);
-                        }
-                    })
-                    .fail(function failRequest() {
-                        var batch;
-                        var i;
+							// Resolve batch
+							batch.resolve.apply(batch, queries);
+						}
+					})
+					.fail(function failRequest() {
+						var batch;
+						var i;
 
-                        // Iterate batches
-                        for (i = batches[LENGTH]; i--;) {
-                            batch = batches[i];
+						// Iterate batches
+						for (i = batches[LENGTH]; i--;) {
+							batch = batches[i];
 
-                            // Reject (with original queries as argument)
-                            batch.reject.apply(batch, batch[QUERIES]);
-                        }
-                    })
-                    .progress(deferred.notify);
-            }, 200);
+							// Reject (with original queries as argument)
+							batch.reject.apply(batch, batch[QUERIES]);
+						}
+					})
+					.progress(deferred.notify);
+			}, 200);
 
-            if (deferred) {
-                deferred.resolve();
-            }
-        },
+			if (deferred) {
+				deferred.resolve();
+			}
+		},
 
-        "sig/stop" : function stop(signal, deferred) {
-            var me = this;
+		"sig/stop" : function stop(signal, deferred) {
+			var me = this;
 
-            // Only do this if we have an interval
-            if (INTERVAL in me) {
-                // Clear interval
-                clearInterval(me[INTERVAL]);
+			// Only do this if we have an interval
+			if (INTERVAL in me) {
+				// Clear interval
+				clearInterval(me[INTERVAL]);
 
-                // Reset interval
-                delete me[INTERVAL];
-            }
+				// Reset interval
+				delete me[INTERVAL];
+			}
 
-            if (deferred) {
-                deferred.resolve();
-            }
-        },
+			if (deferred) {
+				deferred.resolve();
+			}
+		},
 
-        "hub/query" : function query(topic /* query, query, query, .., */, deferred) {
-            var me = this;
-            var length = arguments[LENGTH] - 1;
-            var batches = me[BATCHES];
-            var cache = me[CACHE];
+		"hub/query" : function query(topic /* query, query, query, .., */, deferred) {
+			var me = this;
+			var length = arguments[LENGTH] - 1;
+			var batches = me[BATCHES];
+			var cache = me[CACHE];
 
-            // Slice and flatten queries
-            var queries = CONCAT.apply(ARRAY_PROTO, SLICE.call(arguments, 1, length));
+			// Slice and flatten queries
+			var queries = CONCAT.apply(ARRAY_PROTO, SLICE.call(arguments, 1, length));
 
-            // Update deferred to be the last argument
-            deferred = arguments[length];
+			// Update deferred to be the last argument
+			deferred = arguments[length];
 
-            // Deferred batch
-            Deferred(function deferredBatch(batch) {
-                var query;
-                var q = [];
-                var id = [];
-                var ast;
-                var i;
-                var j;
-                var iMax;
+			// Deferred batch
+			Deferred(function deferredBatch(batch) {
+				var query;
+				var q = [];
+				var id = [];
+				var ast;
+				var i;
+				var j;
+				var iMax;
 
-                // Iterate queries
-                for (i = 0, iMax = queries[LENGTH]; i < iMax; i++) {
-                    // Init Query
-                    query = Query(queries[i]);
+				// Iterate queries
+				for (i = 0, iMax = queries[LENGTH]; i < iMax; i++) {
+					// Init Query
+					query = Query(queries[i]);
 
-                    // Get AST
-                    ast = query.ast();
+					// Get AST
+					ast = query.ast();
 
-                    // If we have an ID
-                    if (ast[LENGTH] > 0) {
-                        // Store raw ID
-                        id[i] = ast[0][RAW];
-                    }
+					// If we have an ID
+					if (ast[LENGTH] > 0) {
+						// Store raw ID
+						id[i] = ast[0][RAW];
+					}
 
-                    // Get reduced AST
-                    ast = query.reduce(cache).ast();
+					// Get reduced AST
+					ast = query.reduce(cache).ast();
 
-                    // Step backwards through AST
-                    for (j = ast[LENGTH]; j-- > 0;) {
-                        // If this op is not resolved
-                        if (!ast[j][RESOLVED]) {
-                            //  Add string version of reduced query to q
-                            PUSH.call(q, query.rewrite())
-                            break;
-                        }
-                    }
-                }
+					// Step backwards through AST
+					for (j = ast[LENGTH]; j-- > 0;) {
+						// If this op is not resolved
+						if (!ast[j][RESOLVED]) {
+							//  Add string version of reduced query to q
+							PUSH.call(q, query.rewrite())
+							break;
+						}
+					}
+				}
 
-                // If all queries were fully reduced, we can quick resolve
-                if (q[LENGTH] === 0) {
-                    // Iterate queries
-                    for (i = 0; i < iMax; i++) {
-                        // If we have a corresponding ID, fetch from cache
-                        if (i in id) {
-                            queries[i] = cache[id[i]];
-                        }
-                    }
+				// If all queries were fully reduced, we can quick resolve
+				if (q[LENGTH] === 0) {
+					// Iterate queries
+					for (i = 0; i < iMax; i++) {
+						// If we have a corresponding ID, fetch from cache
+						if (i in id) {
+							queries[i] = cache[id[i]];
+						}
+					}
 
-                    // Resolve batch
-                    batch.resolve.apply(batch, queries);
-                }
-                else {
-                    // Store properties on batch
-                    batch[TOPIC] = topic;
-                    batch[QUERIES] = queries;
-                    batch[ID] = id;
-                    batch[Q] = q;
+					// Resolve batch
+					batch.resolve.apply(batch, queries);
+				}
+				else {
+					// Store properties on batch
+					batch[TOPIC] = topic;
+					batch[QUERIES] = queries;
+					batch[ID] = id;
+					batch[Q] = q;
 
-                    // Add batch to batches
-                    batches.push(batch);
-                }
-            })
-            .then(deferred.resolve, deferred.reject, deferred.notify);
-        }
-    });
+					// Add batch to batches
+					batches.push(batch);
+				}
+			})
+			.then(deferred.resolve, deferred.reject, deferred.notify);
+		}
+	});
 });
