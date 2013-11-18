58091a076101cf312744f2295d2a2b98d2155a93
Garry Yao <yaojun85@gmail.com>
2013-11-18 10:45:06 +0800

    Never update the expiration from a collapsed object.

--- cache/component.js
+++ cache/component.js
@@ -68,6 +68,11 @@ define([ "troopjs-core/component/base" ], function CacheModule(Component) {
 			// In cache, get it!
 			if (id in me) {
 				result = me[id];
+
+				// Bypass collapsed object that already exists in cache.
+				if(node[_COLLAPSED] === true)
+					return result;
+
 				break cache;
 			}
 
@@ -172,12 +177,13 @@ define([ "troopjs-core/component/base" ], function CacheModule(Component) {
 			}
 
 			add : {
-				// Update expiration time
-				result[_EXPIRES] = expires;
 
 				// Collapsed object should not be collected by GC.
 				if(result[_COLLAPSED] === true)
 					break add;
+
+				// Update expiration time
+				result[_EXPIRES] = expires;
 
 				// Existing generation
 				if (expires in generations) {
